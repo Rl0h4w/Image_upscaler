8f41abaf00540c2bce662833338148acf2d93361
Rlohaw <rl0h4w@gmail.com>
2024-07-12 23:25:27 +0300

    updated to perfect with paralleling and optimizing memory usage with iterators

--- loader_videos_YT.py
+++ loader_videos_YT.py
@@ -1,82 +1,73 @@
 import os
 import re
 import time
+import pytube
+import bs4
 import random
-import asyncio
-import aiohttp
+import threading
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
-from pytube import YouTube
-from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
 
-MAX_SIZE_MB = 50_000
-
-def get_total_size(path):
-    total_size = 0
-    for dirpath, dirnames, filenames in os.walk(path):
-        for f in filenames:
-            fp = os.path.join(dirpath, f)
-            total_size += os.path.getsize(fp)
-    return total_size
 
 def process_url(channel_url):
     channel_url = channel_url.rstrip('/videos') + "/videos"
     options = Options()
-    options.add_argument('--headless=new')
-    options.add_argument('--disable-extensions')
-    options.add_argument('--disable-notifications')
-    options.add_argument('--disable-popup-blocking')
-    service = Service(ChromeDriverManager().install())
-    with webdriver.Chrome(service=service, options=options) as driver:
+    options.add_argument('--headless')
+    service = ChromeService(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+    
+    try:
         driver.get(channel_url)
         last_height = driver.execute_script("return document.documentElement.scrollHeight")
         
         while True:
             driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
-            time.sleep(random.uniform(0.1, 3))  
+            time.sleep(random.uniform(0.5, 1.5))
             new_height = driver.execute_script("return document.documentElement.scrollHeight")
             if new_height == last_height:
                 break
             last_height = new_height
 
-        soup = BeautifulSoup(driver.page_source, "lxml")
+        soup = bs4.BeautifulSoup(driver.page_source, "lxml")
         urls = set(filter(lambda x: x.startswith("/watch"), map(lambda x: x["href"], soup.find_all("a", href=True))))
+    finally:
+        driver.quit()
 
-    return list(map(lambda x: "https://www.youtube.com" + x, urls))
+    return ["https://www.youtube.com" + x for x in urls]
 
-async def download_video(session, url, output_path):
-    global current_size
+def download_video(url, output_path):
     try:
         name = re.search("v=(.+)", url).group(1)
-        yt = YouTube(url, use_oauth=True, allow_oauth_cache=True)
+        yt = pytube.YouTube(url, use_oauth=True, allow_oauth_cache=True)
         stream = yt.streams.filter(adaptive=True, res="144p").first()
         if stream:
-            video_size = stream.filesize
-            if (current_size + video_size) <= MAX_SIZE_MB * 1024 * 1024:
-                loop = asyncio.get_event_loop()
-                await loop.run_in_executor(None, stream.download, output_path)
-                current_size += video_size
-            else:
-                print(f"Skipping {url} due to size limit.")
+            print(f"Downloading {name}")
+            stream.download(output_path=output_path, skip_existing=True)
+            print("Done!")
+        else:
+            print(f"Error: {url}")
     except Exception as e:
         print(f"Error downloading {url}: {e}")
 
-async def process_channel(session, channel_url, output_path):
-    video_urls = process_url(channel_url.strip())
-    tasks = [download_video(session, url, output_path) for url in video_urls]
-    await asyncio.gather(*tasks)
-
-async def download():
-    global current_size
+def download():
     if not os.path.exists("data/videos"):
         os.makedirs("data/videos")
-
-    current_size = get_total_size("data/videos")
-    async with aiohttp.ClientSession() as session:
-        with open("data/channels_urls.txt") as file:
-            channels_urls = file.readlines()
+    with open("data/channels_urls.txt") as file:
+        channels_urls = file.readlines()
         
-        tasks = [process_channel(session, channel_url, "data/videos") for channel_url in channels_urls]
-        await asyncio.gather(*tasks)
+    for channel_url in channels_urls:
+        video_urls = process_url(channel_url.strip())
+        threads = []
+        for url in video_urls:
+            t = threading.Thread(target=download_video, args=(url, "data/videos"))
+            t.start()
+            threads.append(t)
+        
+        for t in threads:
+            t.join()
+            
+if __name__=="__main__":
+    download()
--- main.py
+++ main.py
@@ -3,48 +3,53 @@ import random
 import cv2
 import numpy as np
 import tensorflow as tf
-import keras
-from keras import layers, Model
 from sklearn.preprocessing import StandardScaler
 from joblib import dump, load
 import multiprocessing
 from tqdm import tqdm
-import asyncio
 import loader_videos_YT
-from edsr import make_model
+from models import get_combined_model
+
+x_shape = 32
+scale = 4
+y_shape = x_shape * scale
 
 def extract_frames_from_video(video_path, frame_dir):
     video_name = os.path.splitext(os.path.basename(video_path))[0]
     cap = cv2.VideoCapture(video_path)
-    frame_count = 0
     
     if not cap.isOpened():
-        print(f"Ошибка: Невозможно открыть видеофайл {video_path}")
+        print(f"Error: Unable to open video file {video_path}")
         return
-    
+
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    
-    with tqdm(total=total_frames, desc=f"Обработка {video_name}") as pbar:
-        while cap.isOpened():
+    frame_paths = []
+
+    with tqdm(total=total_frames, desc=f"Processing {video_name}") as pbar:
+        for frame_count in range(total_frames):
             ret, frame = cap.read()
             if not ret:
                 break
             frame_path = os.path.join(frame_dir, f"{video_name}_{frame_count:06d}.png")
-            cv2.imwrite(frame_path, frame)
-            frame_count += 1
+            cv2.imwrite(frame_path, frame, [cv2.IMWRITE_PNG_COMPRESSION, 9])  
+            frame_paths.append(frame_path)
             pbar.update(1)
     
     cap.release()
     cv2.destroyAllWindows()
     
-    if frame_count > 0:
-        os.remove(video_path)
+    if frame_paths:
+        # os.remove(video_path)
+        pass
     else:
-        print(f"Ошибка: Не удалось извлечь кадры из {video_path}")
+        print(f"Error: Unable to extract frames from {video_path}")
 
 def extract_frames(video_dir="data/videos", frame_dir="data/frames"):
-    if not os.path.exists(frame_dir):
-        os.makedirs(frame_dir)
+    if not os.path.exists(video_dir):
+        print(f"Error: Directory {video_dir} does not exist.")
+        return
+    
+    os.makedirs(frame_dir, exist_ok=True)
     
     files = [os.path.join(video_dir, f) for f in os.listdir(video_dir) if os.path.isfile(os.path.join(video_dir, f))]
     
@@ -53,25 +58,36 @@ def extract_frames(video_dir="data/videos", frame_dir="data/frames"):
 
 def downscale_and_add_noise(image):
     brightness_factor = random.uniform(0.5, 1.5)
-    bright_image = cv2.convertScaleAbs(image, alpha=brightness_factor, beta=0)
+    image = cv2.convertScaleAbs(image, alpha=brightness_factor, beta=0)
     
-    hsv_image = cv2.cvtColor(bright_image, cv2.COLOR_BGR2HSV)
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     saturation_factor = random.uniform(0.5, 1.5)
     hsv_image[:, :, 1] = np.clip(hsv_image[:, :, 1] * saturation_factor, 0, 255)
-    sat_image = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR)
+    image = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR)
     
     contrast_factor = random.uniform(0.5, 1.5)
-    contrast_image = cv2.convertScaleAbs(sat_image, alpha=contrast_factor, beta=0)
+    image = cv2.convertScaleAbs(image, alpha=contrast_factor, beta=0)
     
     noise_factor = random.uniform(0, 0.05)
-    noise = np.random.randn(*contrast_image.shape) * 255 * noise_factor
-    noisy_image = cv2.add(contrast_image, noise.astype(np.uint8))
+    noise = np.random.randn(*image.shape) * 255 * noise_factor
+    image = cv2.add(image, noise.astype(np.uint8))
+    
+    if random.random() < 0.5:  
+        ksize = random.choice([(3, 3), (5, 5), (7, 7)])
+        image = cv2.GaussianBlur(image, ksize, 0)
+    
+    if random.random() < 0.5:  
+        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), random.randint(10, 50)]
+        _, encoded_image = cv2.imencode('.jpg', image, encode_param)
+        image = cv2.imdecode(encoded_image, 1)
     
     scale_factor = random.uniform(0.2, 1.0)
-    height, width = noisy_image.shape[:2]
-    low_res_image = cv2.resize(noisy_image, (int(width * scale_factor), int(height * scale_factor)))
+    height, width = image.shape[:2]
+    image = cv2.resize(image, (int(width * scale_factor), int(height * scale_factor)))
     
-    return low_res_image
+    image = cv2.resize(image, (width, height))
+    
+    return image
 
 def data_augmentation():
     return tf.keras.Sequential([
@@ -80,50 +96,59 @@ def data_augmentation():
         tf.keras.layers.RandomZoom(0.2),
     ])
 
-def standardize_frames(frames, is_val=False, y_or_x=None):
-    frames = frames.astype(np.float32) / 255.0 
-    if not is_val:
-        scaler = StandardScaler()
-        standardized_data = scaler.fit_transform(frames.reshape(-1, frames.shape[-1]))
-        dump(scaler, f"scaler_model{y_or_x}.joblib")
-    else:
-        scaler = load(f"scaler_model{y_or_x}.joblib")
-        standardized_data = scaler.transform(frames.reshape(-1, frames.shape[-1]))
-    return standardized_data.reshape(frames.shape)
+def standardize_frame(frame, scaler):
+    frame = frame.astype(np.float32) / 255.0
+    return scaler.transform(frame.reshape(-1, frame.shape[-1])).reshape(frame.shape)
 
-def create_dataset(image_dir="data/frames"):
-    x_data = []
-    y_data = []
-    
-    files = [f for f in os.listdir(image_dir) if os.path.isfile(os.path.join(image_dir, f))]
-    
-    for file_name in files:
-        image_path = os.path.join(image_dir, file_name)
+def frame_generator(files, batch_size, scaler, is_val=False):
+    while True:
+        random.shuffle(files)
+        for start in range(0, len(files), batch_size):
+            x_data = []
+            y_data = []
+            end = min(start + batch_size, len(files))
+            for file_name in files[start:end]:
+                image_path = file_name
+                image = cv2.imread(image_path)
+                if image is None:
+                    continue
+                
+                y_image = cv2.resize(image, (y_shape, y_shape))
+                x_image = downscale_and_add_noise(y_image.copy())
+                x_image = cv2.resize(x_image, (x_shape, x_shape))
+                
+                x_image = standardize_frame(x_image, scaler)
+                y_image = standardize_frame(y_image, scaler)
+                
+                x_data.append(x_image)
+                y_data.append(y_image)
+            
+            x_data = np.array(x_data)
+            y_data = np.array(y_data)
+            
+            yield x_data, y_data
+
+def split_file_paths(files, test_size=0.2):
+    split_idx = int(len(files) * (1 - test_size))
+    return files[:split_idx], files[split_idx:]
+
+def prepare_scaler(image_dir):
+    files = [os.path.join(image_dir, f) for f in os.listdir(image_dir) if os.path.isfile(os.path.join(image_dir, f))]
+    sample_images = []
+    for file_name in files[:1000]: 
+        image_path = file_name
         image = cv2.imread(image_path)
         if image is None:
             continue
-        
-        y_image = cv2.resize(image, (144, 144)) 
-        x_image = downscale_and_add_noise(y_image)  
-        x_image = cv2.resize(x_image, (36, 36))  
-        
-        x_data.append(x_image)
-        y_data.append(y_image)
-    
-    x_data = np.array(x_data)
-    y_data = np.array(y_data)
-        
-    augmentation = data_augmentation()
-    
-    x_data_aug = augmentation(x_data)
-    y_data_aug = augmentation(y_data)
-    return x_data_aug, y_data_aug
-
-def split_dataset(x_data, y_data, test_size=0.2):
-    split_idx = int(len(x_data) * (1 - test_size))
-    x_train, x_val = x_data[:split_idx], x_data[split_idx:]
-    y_train, y_val = y_data[:split_idx], y_data[split_idx:]
-    return x_train, x_val, y_train, y_val
+        y_image = cv2.resize(image, (y_shape, y_shape))
+        x_image = downscale_and_add_noise(y_image.copy())
+        x_image = cv2.resize(x_image, (x_shape, x_shape))
+        sample_images.append(x_image)
+        sample_images.append(y_image)
+    sample_images = [cv2.resize(img, (x_shape, x_shape)) if img.shape[:2] == (y_shape, y_shape) else img for img in sample_images]
+    sample_images = np.array(sample_images).astype(np.float32) / 255.0
+    scaler = StandardScaler().fit(sample_images.reshape(-1, sample_images.shape[-1]))
+    return scaler
 
 def psnr_metric(y_true, y_pred):
     return tf.image.psnr(y_true, y_pred, max_val=1.0)
@@ -131,31 +156,38 @@ def psnr_metric(y_true, y_pred):
 def ssim_metric(y_true, y_pred):
     return tf.image.ssim(y_true, y_pred, max_val=1.0)
 
-
-
 if __name__ == "__main__":
-    asyncio.run(loader_videos_YT.download())
-    extract_frames()
+    if not os.path.exists("data/videos") and not os.path.exists("data/frames"):
+        loader_videos_YT.download()
+        extract_frames()
     
-    x_data, y_data = create_dataset()
-    x_train, x_val, y_train, y_val = split_dataset(x_data, y_data)
+    batch_size = 32
+    image_dir = "data/frames"
     
-    x_train = standardize_frames(x_train, is_val=False, y_or_x="x")
-    x_val = standardize_frames(x_val, is_val=True, y_or_x="x")
-    y_train = standardize_frames(y_train, is_val=False, y_or_x="y")
-    y_val = standardize_frames(y_val, is_val=True, y_or_x="y")
+    scaler = prepare_scaler(image_dir)
     
-    checkpoint_cb = tf.keras.callbacks.ModelCheckpoint("edsr_model_best.h5", save_best_only=True)
+    files = [os.path.join(image_dir, f) for f in os.listdir(image_dir) if os.path.isfile(os.path.join(image_dir, f))]
+    
+    train_files, val_files = split_file_paths(files, test_size=0.2)
+    
+    train_gen = frame_generator(train_files, batch_size, scaler, is_val=False)
+    val_gen = frame_generator(val_files, batch_size, scaler, is_val=True)
+    
+    checkpoint_cb = tf.keras.callbacks.ModelCheckpoint("unet_model_best.keras", save_best_only=True)
     early_stopping_cb = tf.keras.callbacks.EarlyStopping(patience=10, restore_best_weights=True)
     reduce_lr_cb = tf.keras.callbacks.ReduceLROnPlateau(factor=0.5, patience=5, min_lr=0.00001)
     tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir="./logs")
     callbacks = [checkpoint_cb, early_stopping_cb, reduce_lr_cb, tensorboard_cb]
     
-    edsr_model = make_model(num_filters=64, num_of_residual_blocks=16)
-    optim_edsr = keras.optimizers.Adam(
-    learning_rate=keras.optimizers.schedules.PiecewiseConstantDecay(
-        boundaries=[5000], values=[1e-4, 5e-5]
+    model = get_combined_model(scale=scale, input_shape=(x_shape, x_shape, 3))
+    
+    model.compile(optimizer='adam', loss='mse', metrics=["mae", "mse", "accuracy", psnr_metric, ssim_metric])
+    
+    model.fit(
+        train_gen,
+        steps_per_epoch=len(train_files) // batch_size,
+        validation_data=val_gen,
+        validation_steps=len(val_files) // batch_size,
+        epochs=50,
+        callbacks=callbacks
     )
-)
-    edsr_model.compile(optimizer=optim_edsr, loss="mae", metrics=[psnr_metric, ssim_metric, "accuracy", "mse"])
-    edsr_model.fit(x_train, y_train, validation_data = [x_val, y_val], batch_size=2, epochs=100, steps_per_epoch=200, callbacks=callbacks)
